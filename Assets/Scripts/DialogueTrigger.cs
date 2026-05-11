7cbcc558f24d18b472428b506732c984f51d3c05
MateoP <m.pontaud@nouvellesimages.xyz>
2026-05-11 15:59:45 +0200

    commit du 11/05/26

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+    private bool isInRange;
+    private Text interactUI;
+    
+    void Awake()
+    {
+        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<Text>();
+    }
+    
+    void Update()
+    {
+        if (isInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            TriggerDialogue();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.enabled = true;
+            isInRange = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+            interactUI.enabled = false;
+            DialogueManager.instance.EndDialogue();
+        }
+    }
+
+    void TriggerDialogue()
+    {
+        DialogueManager.instance.StartDialogue(dialogue);
+    }
+}
